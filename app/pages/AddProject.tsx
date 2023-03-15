08001d93c181726ac06ddb3a5feaa6a28d79d63e
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-03-15 14:41:12 -0700

    added navbar

---
+++ app/pages/AddProject.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+const AddProject = () => {
+    return (
+        <Text>Add new project</Text>
+    )
+}
+
+export default AddProject;
