6fef54b61230125f64d836d41b67d2cc291bdd91
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-03-16 20:02:26 -0700

    get request functioning for projects

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [projects, setProjects] = useState<any[]>([]);
+    console.log(projects)
+
+    useEffect(() => {
+        axios.get(`http://192.168.1.150:3001/project`)
+          .then((result: any) => {
+            setProjects(result.data);
+          },
+            (error) => {
+              console.log(error);
+            })
+      }, [])
+
     return (
         <View>
-            <Text>Home Page</Text>
+            <Text>{JSON.stringify(projects[0])}</Text>
         </View>
     )
 }
