5e7aec90a96f880643be6c84ef65f1f884d7e2f9
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-03-22 12:39:45 -0700

    started building out add project tab

--- app/pages/AddProject.tsx
+++ app/pages/AddProject.tsx
@@ -1,9 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from "react";
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from "react";
+// import Date from '../Components/Date';
+import Gauge from '../Components/Gauge';
 
 const AddProject = () => {
+    const [project, setProject] = useState({
+        name: '',
+        needle_size: 0,
+        gauge: 0,
+        user_id: 0,
+        date_started: '',
+        date_finished: ''
+    });
+
+    const [yarn, setYarn] = useState({
+        yarn_name: '',
+        weight: 0,
+        yardage: 0,
+        color: '',
+        project_id: 0
+    });
+
     return (
-        <Text>Add new project</Text>
+        <View>
+            <Text>Add new project</Text>
+            <Gauge />
+        </View>
     )
 }
 
