8af20795dc290c9e5a496073c1c6f659d5d29ff9
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-03-28 11:33:13 -0700

    modal added

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal } from 'react-native';
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 const Home = () => {
@@ -8,20 +8,55 @@ const Home = () => {
     console.log(projects)
 
     useEffect(() => {
-        axios.get(`http://192.168.1.150:3001/project`)
-          .then((result: any) => {
-            setProjects(result.data);
-          },
-            (error) => {
-              console.log(error);
-            })
+      async function getProjects() {
+        try {
+          let response = await axios.get(`http://10.7.24.27:3001/project`);
+          if (response.data) {
+            setProjects(response.data);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      getProjects();
       }, [])
 
-    return (
+      const modalRef = useRef(null);
+      const [isModalVisible, setModalVisible] = useState(false);
+
+      const showModal = () => {
+        setModalVisible(true);
+      };
+
+      const hideModal = () => {
+        setModalVisible(false);
+      };
+
+      const toggleModal = () => {
+        if (modalRef.current) {
+          modalRef.current.isVisible ? hideModal() : showModal();
+        }
+      };
+
+      return (
         <View>
-            <Text>{JSON.stringify(projects[0])}</Text>
+          {projects.map((project) => (
+            <View key={project.id}>
+              <Text onPress={toggleModal}>
+                {JSON.stringify(project.name)}
+              </Text>
+              <Modal
+                ref={modalRef}
+                visible={isModalVisible}
+              >
+                <Text>{JSON.stringify(project.name)}</Text>
+                <Text onPress={hideModal}>Close</Text>
+              </Modal>
+            </View>
+          ))}
         </View>
-    )
+      );
 }
 
 export default Home;
