61d2b4a168b361023f21727bd1acc0082e42cb09
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-04-03 08:32:23 -0700

    imported form components

--- app/pages/AddProject.tsx
+++ app/pages/AddProject.tsx
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React, { useState } from "react";
 // import Date from '../Components/Date';
 import Gauge from '../Components/Gauge';
+import Tool from '../Components/Tool';
+import Yarn from '../Components/Yarn';
+import axios from 'axios';
 
 const AddProject = () => {
     const [project, setProject] = useState({
@@ -21,10 +24,40 @@ const AddProject = () => {
         project_id: 0
     });
 
+    const handleAddProject = async (e: any) => {
+        e.preventDefault();
+        let res = await axios.post('http://localhost:3001/project', {
+          project, yarn
+        })
+        console.log(res);
+        console.log(project)
+        console.log("yarn info")
+      }
+
+      const handleProjectChange = (e: any) => {
+        e.preventDefault();
+        setProject({
+          ...project,
+          [e.target.name]: e.target.value
+        });
+      }
+
+
+      const handleYarnChange = (e: any) => {
+        e.preventDefault();
+        setYarn({
+          ...yarn,
+          [e.target.name]: e.target.value
+        });
+      };
+
     return (
         <View>
             <Text>Add new project</Text>
             <Gauge />
+            <Tool />
+            <Yarn />
+            <Text onPress={handleAddProject}>Add Project</Text>
         </View>
     )
 }
