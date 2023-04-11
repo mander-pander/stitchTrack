384a07a80d99c686c9339aa2c733f1730af656f8
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-04-11 10:56:26 -0700

    styling navbar with screenOptions

--- app/App.js
+++ app/App.js
@@ -1,30 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Home from './pages/Home';
 import AddProject from './pages/AddProject';
-import Yarn from './pages/Yarn';
+import YarnTab from './pages/YarnTab';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="AddProject" component={AddProject} />
-        <Tab.Screen name="Yarn" component={Yarn} />
+    <NavigationContainer >
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            if (route.name === 'Home') {
+              return (
+                <Ionicons
+                  name={'list-outline'}
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'AddProject') {
+              return (
+                <Ionicons
+                  name={'add-outline'}
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Yarn') {
+              return (
+                <Ionicons
+                  name={'bookmark-outline'}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+          tabBarInactiveTintColor: '#450920',
+          tabBarActiveTintColor: '#F9DBBD',
+          tabBarStyle:
+          {
+            backgroundColor: '#DA627D',
+            borderRadius: 20,
+            position: 'absolute'
+
+
+          },
+        })}
+        initialRouteName="Home"
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+        />
+        <Tab.Screen
+          name="AddProject"
+          component={AddProject}
+        />
+        <Tab.Screen
+          name="Yarn"
+          component={YarnTab}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
