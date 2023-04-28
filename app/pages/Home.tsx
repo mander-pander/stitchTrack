ec230a5fcab371d78657f9643045b71e1156c997
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-04-28 13:48:02 -0700

    Broke out card with modal into its own component

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -5,75 +5,83 @@ import axios from 'axios';
 import { Card } from '@rneui/themed';
 
 const Home = () => {
-    const [projects, setProjects] = useState<any[]>([]);
-    console.log(projects)
+  const [projects, setProjects] = useState<any[]>([]);
 
-    useEffect(() => {
-      async function getProjects() {
-        try {
-          let response = await axios.get(`http://10.113.6.118:3001/project`);
-          if (response.data) {
-            setProjects(response.data);
-          }
-        } catch (err) {
-          console.log(err);
+  useEffect(() => {
+    async function getProjects() {
+      try {
+        let response = await axios.get(`http://192.168.1.150:3001/project`);
+        if (response.data) {
+          setProjects(response.data);
         }
+      } catch (err) {
+        setProjects([null]);
       }
-      getProjects();
-      }, []);
+    }
+    getProjects();
+  }, []);
 
-      const modalRef = useRef(null);
-      const [isModalVisible, setModalVisible] = useState(false);
 
-      const showModal = () => {
-        setModalVisible(true);
-      };
 
-      const hideModal = () => {
-        setModalVisible(false);
-      };
+  return (
+    <View style={styles.container}>
+      {projects.map((project) => <CardWithModal project={project} />)}
+    </View>
+  );
+}
 
-      const toggleModal = () => {
-        if (modalRef.current) {
-          modalRef.current.isVisible ? hideModal() : showModal();
-        }
-      };
+const CardWithModal = (props) => {
+  const {
+    project
+  } = props;
 
-      return (
-        <View style={styles.container}>
-          {projects.map((project) => (
-            <Card key={project.id}>
-              <Text style={styles.projectList} onPress={toggleModal}>
-                {project.name}
-              </Text>
-              <Modal
-                ref={modalRef}
-                visible={isModalVisible}
-              >
+  const modalRef = useRef(null);
+  const [isModalVisible, setModalVisible] = useState(false);
 
-                <View style={styles.modalInfo}>
-                <Text>
-                  Project Name: {project.name}
-                </Text>
-                <Text>
-                  Needle Size: {project.needle_size}
-                </Text>
-                <Text>
-                  Gauge: {project.gauge}
-                </Text>
-                <Text>
-                  Notes: {project.notes}
-                </Text>
-                  <View style={styles.buttons}>
-                    <Button color="#450920" title="Go back to projects" onPress={hideModal} />
-                    <Button color="#450920" title="Edit project details" />
-                  </View>
-                </View>
-              </Modal>
-            </Card>
-          ))}
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+  };
+
+  const toggleModal = () => {
+    if (modalRef.current) {
+      modalRef.current.isVisible ? hideModal() : showModal();
+    }
+  };
+  return (
+    <Card key={project.id}>
+      <Text style={styles.projectList} onPress={toggleModal}>
+        {project.name}
+      </Text>
+      {/* modal pop up */}
+      <Modal
+        ref={modalRef}
+        visible={isModalVisible}
+      >
+        <View style={styles.modalInfo}>
+          <Text>
+            Project Name: {project.name}
+          </Text>
+          <Text>
+            Needle Size: {project.needle_size}
+          </Text>
+          <Text>
+            Gauge: {project.gauge}
+          </Text>
+          <Text>
+            Notes: {project.notes}
+          </Text>
+          <View style={styles.buttons}>
+            <Button color="#450920" title="Go back to projects" onPress={hideModal} />
+            <Button color="#450920" title="Edit project details" />
+          </View>
         </View>
-      );
+      </Modal>
+    </Card>
+  );
 }
 
 const styles = StyleSheet.create({
