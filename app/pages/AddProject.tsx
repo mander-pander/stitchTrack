47ec22e349c84b3f7c61521017826bbd1c916782
Amanda Godsey <amanda.d.godsey@gmail.com>
2023-10-06 14:45:29 -0700

    styling changes, changed variable names for better code readability

--- app/pages/AddProject.tsx
+++ app/pages/AddProject.tsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 
 const AddProject = () => {
-    const [project, setProject] = useState({
+    const [newProject, setNewProject] = useState({
         name: '',
         needle_size: 0,
         gauge: 0,
@@ -25,36 +25,36 @@ const AddProject = () => {
         project_id: 0
     });
 
+    const handleProjectChange = (e: any) => {
+      e.preventDefault();
+      setNewProject({
+        ...newProject,
+        [e.target.name]: e.target.value
+      });
+    }
+
+    const handleYarnChange = (e: any) => {
+      e.preventDefault();
+      setYarn({
+        ...yarn,
+        [e.target.name]: e.target.value
+      });
+    };
+
     const handleAddProject = async (e: any) => {
         e.preventDefault();
-        if (project.name.trim() === '') {
+        if (newProject.name.trim() === '') {
           Alert.alert('Error', 'Please enter a name for project');
         } else {
           let res = await axios.post('http://192.168.1.150:3001/project', {
-            project, yarn
+            newProject, yarn
           })
           console.log(res);
-          console.log(project)
+          console.log(newProject)
           console.log("yarn info")
         }
     }
 
-      const handleProjectChange = (e: any) => {
-        e.preventDefault();
-        setProject({
-          ...project,
-          [e.target.name]: e.target.value
-        });
-      }
-
-
-      const handleYarnChange = (e: any) => {
-        e.preventDefault();
-        setYarn({
-          ...yarn,
-          [e.target.name]: e.target.value
-        });
-      };
 
     return (
         <View style={styles.container}>
@@ -63,11 +63,11 @@ const AddProject = () => {
               <Text style={styles.label}>Project Name:</Text>
               <TextInput
                   style={styles.input}
-                  value={project.name}
+                  value={newProject.name}
                   onChange={handleProjectChange}
               />
-              <Gauge gauge={project.gauge} onChange={handleProjectChange} />
-              <Tool needle_size={project.needle_size} onChange={handleProjectChange}/>
+              <Gauge gauge={newProject.gauge} onChange={handleProjectChange} />
+              <Tool needle_size={newProject.needle_size} onChange={handleProjectChange}/>
               <Yarn yarn_name={yarn.yarn_name} weight={yarn.weight} yardage={yarn.yardage} color={yarn.color} onChange={handleYarnChange} />
               <View style={styles.submitBtn} >
               <Button title="Add Project" onPress={handleAddProject} color="#450920" />
@@ -79,23 +79,24 @@ const AddProject = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F9DBBD",
+    backgroundColor: "#FAFAFF",
     minHeight: "100%",
-    color: "#450920",
+    color: "#1E2749",
+    padding: '2%',
   },
   title: {
     alignSelf: "center",
-    color: "#450920",
+    color: "#1E2749",
     fontWeight: "bold",
     fontSize: 19
   },
   label: {
-    color: "#450920",
+    color: "#1E2749",
   },
   input: {
-    borderWidth: 1,
-    borderRadius: 5,
     padding: 10,
+    borderColor: "#1E2749",
+    borderBottomWidth: 1,
 },
   submitBtn: {
     alignSelf: "center",
